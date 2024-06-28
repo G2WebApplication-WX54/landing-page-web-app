0ae5fdf7e47d651f4e12773bb552523d96137811
Anatoly69420 <u202211813@upc.edu.pe>
2024-06-27 23:25:56 -0500

    feat: Updated CSS for responsive iframes and improved section padding

--- assets/menu.js
+++ assets/menu.js
@@ -6,72 +6,90 @@ function closeNav() {
     document.getElementById("mobile-menu").style.width = "0%";
 }
 
-const about = document.querySelector("#about");
+const aboutUs = document.querySelector("#about-us");
 const app = document.querySelector("#app");
 const services = document.querySelector("#services");
-const planes = document.querySelector("#plans");
+const plans = document.querySelector("#plans");
+const aboutProduct = document.querySelector("#about-product");
+const aboutTeam = document.querySelector("#about-team");
 const contact = document.querySelector("#contact");
 
-about.addEventListener("click", (n) => {
-    n.preventDefault();
-    const selectionN = document.querySelector("#about-section");
-    selectionN.scrollIntoView({ behavior: "smooth" });
+aboutUs.addEventListener("click", (e) => {
+    e.preventDefault();
+    const aboutSection = document.querySelector("#about-us-section");
+    aboutSection.scrollIntoView({ behavior: "smooth" });
 });
-app.addEventListener("click", (cr) => {
-    cr.preventDefault();
-    const selectionCr = document.querySelector("#app-section");
-    selectionCr.scrollIntoView({ behavior: "smooth" });
+app.addEventListener("click", (e) => {
+    e.preventDefault();
+    const appSection = document.querySelector("#app-section");
+    appSection.scrollIntoView({ behavior: "smooth" });
 });
-services.addEventListener("click", (s) => {
-    s.preventDefault();
-    const selectionS = document.querySelector("#services-section");
-    selectionS.scrollIntoView({ behavior: "smooth" });
+services.addEventListener("click", (e) => {
+    e.preventDefault();
+    const servicesSection = document.querySelector("#services-section");
+    servicesSection.scrollIntoView({ behavior: "smooth" });
 });
-plans.addEventListener("click", (p) => {
-    p.preventDefault();
-    const selectionP = document.querySelector("#plans-section");
-    selectionP.scrollIntoView({ behavior: "smooth" });
+plans.addEventListener("click", (e) => {
+    e.preventDefault();
+    const plansSection = document.querySelector("#plans-section");
+    plansSection.scrollIntoView({ behavior: "smooth" });
+});
+aboutProduct.addEventListener("click", (e) => {
+    e.preventDefault();
+    const aboutProductSection = document.querySelector("#about-product-section");
+    aboutProductSection.scrollIntoView({ behavior: "smooth" });
+});
+aboutTeam.addEventListener("click", (e) => {
+    e.preventDefault();
+    const aboutTeamSection = document.querySelector("#about-team-section");
+    aboutTeamSection.scrollIntoView({ behavior: "smooth" });
+});
+contact.addEventListener("click", (e) => {
+    e.preventDefault();
+    const contactSection = document.querySelector("#contact-section");
+    contactSection.scrollIntoView({ behavior: "smooth" });
 });
 
-contact.addEventListener("click", (c) => {
-    c.preventDefault();
-    const selectionC = document.querySelector("#contact-section");
-    selectionC.scrollIntoView({ behavior: "smooth" });
-});
-
-const about2 = document.querySelector("#about2");
+const aboutUs2 = document.querySelector("#about-us2");
 const services2 = document.querySelector("#services2");
 const plans2 = document.querySelector("#plans2");
-const app2 = document.querySelector("#app2");
+const aboutProduct2 = document.querySelector("#about-product2");
+const aboutTeam2 = document.querySelector("#about-team2");
 const contact2 = document.querySelector("#contact2");
 
-about2.addEventListener("click", (n) => {
-    n.preventDefault();
-    const selectionN = document.querySelector("#about-section");
-    selectionN.scrollIntoView({ behavior: "smooth" });
+aboutUs2.addEventListener("click", (e) => {
+    e.preventDefault();
+    const aboutSection = document.querySelector("#about-us-section");
+    aboutSection.scrollIntoView({ behavior: "smooth" });
     closeNav();
 });
-app2.addEventListener("click", (cr) => {
-    cr.preventDefault();
-    const selectionCr = document.querySelector("#app-section");
-    selectionCr.scrollIntoView({ behavior: "smooth" });
+services2.addEventListener("click", (e) => {
+    e.preventDefault();
+    const servicesSection = document.querySelector("#services-section");
+    servicesSection.scrollIntoView({ behavior: "smooth" });
     closeNav();
 });
-services2.addEventListener("click", (s) => {
-    s.preventDefault();
-    const selectionS = document.querySelector("#services-section");
-    selectionS.scrollIntoView({ behavior: "smooth" });
+plans2.addEventListener("click", (e) => {
+    e.preventDefault();
+    const plansSection = document.querySelector("#plans-section");
+    plansSection.scrollIntoView({ behavior: "smooth" });
     closeNav();
 });
-plans2.addEventListener("click", (p) => {
-    p.preventDefault();
-    const selectionP = document.querySelector("#plans-section");
-    selectionP.scrollIntoView({ behavior: "smooth" });
+aboutProduct2.addEventListener("click", (e) => {
+    e.preventDefault();
+    const aboutProductSection = document.querySelector("#about-product-section");
+    aboutProductSection.scrollIntoView({ behavior: "smooth" });
     closeNav();
 });
-contact2.addEventListener("click", (c) => {
-    c.preventDefault();
-    const selectionC = document.querySelector("#contact-section");
-    selectionC.scrollIntoView({ behavior: "smooth" });
+aboutTeam2.addEventListener("click", (e) => {
+    e.preventDefault();
+    const aboutTeamSection = document.querySelector("#about-team-section");
+    aboutTeamSection.scrollIntoView({ behavior: "smooth" });
+    closeNav();
+});
+contact2.addEventListener("click", (e) => {
+    e.preventDefault();
+    const contactSection = document.querySelector("#contact-section");
+    contactSection.scrollIntoView({ behavior: "smooth" });
     closeNav();
 });
